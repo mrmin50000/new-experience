c800cdda0b399468a44dc58edd37d13af82f31e5
mrmin50000 <danilast2007@gmail.com>
2025-08-02 20:43:31 +0300

    lmao

--- go/fizzbuzz.go
+++ go/fizzbuzz.go
@@ -4,14 +4,17 @@ import "fmt"
 
 func main() {
 	for i := 1; i <= 100; i++ {
+		if i%5 == 0 && i%3 == 0 {
+			fmt.Println("FizzBuzz")
+			continue
+		}
 		if i%3 == 0 {
 			fmt.Println("Fizz")
 			continue
-		} else if i%5 == 0 {
+		}
+		if i%5 == 0 {
 			fmt.Println("Buzz")
 			continue
-		} else if i%5 == 0 && i%3 == 0 {
-			fmt.Println("FizzBuzz")
 		}
 		fmt.Println(i)
 	}
