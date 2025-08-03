c632677af2a578ed174b169757e477171fd065ce
mrmin50000 <danilast2007@gmail.com>
2025-08-03 15:47:04 +0300

    .

--- go/pointers.go
+++ go/pointers.go
@@ -1,8 +1,27 @@
 package main
 
+import "fmt"
+
 func main() {
 	var a int = 5
 	var p *int = &a
 
 	println(a, p)
+
+	incrementCopy := func(i int) {
+		i++
+	}
+
+	increment := func(i *int) {
+		*i++
+	}
+
+	i := 42
+
+	incrementCopy(i)
+	fmt.Println(i) // 42
+
+	increment(&i)
+	fmt.Println(i) // 43
+
 }
