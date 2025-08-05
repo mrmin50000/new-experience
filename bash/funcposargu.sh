115a6b96efc6db85d8721ffa6a72d05fb92c3140
mrmin50000 <danilast2007@gmail.com>
2025-08-05 16:19:44 +0300

    exit code

--- bash/funcposargu.sh
+++ bash/funcposargu.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 showname(){
 	echo hello $1
+	if [ ${1,,} = mrmin50000 ]; then
+		return 0
+	else
+		return 1
+	fi
 }
-showname mrmin50000
+showname $1
+if [ $? = 1 ]; then
+	echo "idk"
+fi
+
